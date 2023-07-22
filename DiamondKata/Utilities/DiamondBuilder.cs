0f34314f13066b7472cd7db9fcc12b920fc31247
ewand <ewan98davies@gmail.com>
2023-07-22 11:22:48 +0100

    Add tests for unimplemented DiamondBuilder

---
+++ DiamondKata/Utilities/DiamondBuilder.cs
@@ -0,0 +1,28 @@
+﻿using DiamondKata.Model;
+using System.Linq;
+
+namespace DiamondKata.Utilities
+{
+    public class DiamondBuilder : IAsciiArtBuilder<char>
+    {
+        private readonly IAsciiArtBuilder<DiamondLineData> diamondLineBuilder;
+        private readonly IConverter<char, int> characterIndexConverter;
+
+        /// <summary>
+        /// Instantiates an instance of the class <see cref="DiamondBuilder"/>
+        /// </summary>
+        /// <param name="diamondLineBuilder">The line builder to use for constructing lines of the art.</param>
+        public DiamondBuilder(
+            IAsciiArtBuilder<DiamondLineData> diamondLineBuilder,
+            IConverter<char, int> characterIndexConverter)
+        { 
+            this.diamondLineBuilder = diamondLineBuilder;
+            this.characterIndexConverter = characterIndexConverter;
+        }
+
+        public string Build(char input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DiamondKataTests/Utilities/DiamondBuilderTests.cs
@@ -0,0 +1,54 @@
+﻿using DiamondKata.Utilities;
+
+namespace DiamondKataTests.Utilities
+{
+    [TestFixture]
+    public class DiamondBuilderTests
+    {
+        private const string expectedA = @"A";
+
+        private const string expectedB = @" A 
+B B
+ A ";
+
+        private const string expectedC = @"  A  
+ B B 
+C   C
+ B B 
+  A  ";
+
+        private IAsciiArtBuilder<char> diamondBuilder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var characterIndexConverter = new CharacterIndexConverter();
+            diamondBuilder = new DiamondBuilder(
+                new DiamondLineBuilder(characterIndexConverter),
+                characterIndexConverter);
+        }
+
+        [TestCase('A', expectedA)]
+        [TestCase('B', expectedB)]
+        [TestCase('C', expectedC)]
+        public void TestDiamondBuilderOnSmallInputs(char input, string expected)
+        {
+            // Arrange / Act
+            var result = diamondBuilder.Build(input);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase('a')]
+        [TestCase('z')]
+        [TestCase('$')]
+        [TestCase('£')]
+        [TestCase('\n')]
+        [TestCase('*')]
+        public void TestDiamondBuilderThrowsOnInvalidInput(char input)
+        {
+            Assert.Throws<ArgumentException>(() => diamondBuilder.Build(input));
+        }
+    }
+}
